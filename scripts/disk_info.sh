845de927b9a9552e26a993e09af8ecfddc44ca53
Andreas <and_ice86@yahoo.de>
2025-05-05 19:33:07 +0000

    Add disk-info feature
    Provides information about used and available disk storage
    of the root partition e.g. '240G/800G'.

---
+++ scripts/disk_info.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# setting the locale, some users have issues with different locales, this forces the correct one
+export LC_ALL=en_US.UTF-8
+
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $current_dir/utils.sh
+
+get_disk_info()
+{
+  case $(uname -s) in
+    Linux)
+      echo "$(df -h / | tail -1 | awk '{print $3 "/" $2}')"
+      ;;
+
+  esac
+}
+
+main()
+{
+  disk_label=$(get_tmux_option "@dracula-disk-usage-label" "DISK")
+  disk_ratio=$(get_disk_info)
+  echo "$disk_label $disk_ratio"
+}
+
+#run main driver
+main
